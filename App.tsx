7d0b3defe72c2898e67896f9da078fb7952a594e
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-03-11 11:54:18 -0600

    Salvando recurso

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform } from 'react-native';
+import { useNotification } from './src/hooks/useNotification';
+
+async function sendPushNotification(expoPushToken:any) {
+  const message = {
+    to: expoPushToken,
+    sound: 'default',
+    title: 'Este seria el saludo',
+    body: 'Este seria el cuerpo de la notificación',
+    data: { someData: 'goes here' },
+  };
+
+  await fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Accept-encoding': 'gzip, deflate',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  });
+}
 
 export default function App() {
+  const expoPushToken = useNotification();
+  console.log(expoPushToken);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around' }}>
+      <Button
+        title="Press to Send Notification"
+        onPress={async () => {
+          await sendPushNotification(expoPushToken);
+        }}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
